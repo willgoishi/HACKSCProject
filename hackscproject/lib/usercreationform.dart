a138489b4f335cf605c8a0cb314fea406c8d5eff
willgoishi <william3296@mail.fresnostate.edu>
2020-02-01 16:54:36 -0800

    updated user creation form +/ minor refactoring

--- hackscproject/lib/usercreationform.dart
+++ hackscproject/lib/usercreationform.dart
@@ -1,62 +1,111 @@
 import 'package:flutter/material.dart';
 
+void main() => runApp(UserCreationForm());
+
+/// This Widget is the main application widget.
 class UserCreationForm extends StatelessWidget {
- final _formKey = GlobalKey<FormState>();
-
-@override
-Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Herrooo friend"),
+  final _formKey = GlobalKey<FormState>();
+  static const String _title = 'User Profile';
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(_title)),
+        body: MyStatefulWidget(),
       ),
-      body: Container(
-        child: Form(
-    key: _formKey,
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        TextFormField(
-          decoration: const InputDecoration(
-            hintText: 'Enter your email',
-          ),
-          validator: (value) {
-            if (value.isEmpty) {
-              return 'Please enter some text';
-            }
-            return null;
-          },
-          
+    );
+  }
+}
+class MyStatefulWidget extends StatefulWidget {
+  MyStatefulWidget({Key key}) : super(key: key);
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  final _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding : EdgeInsets.all(10),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Enter name or Alias ',
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'This section is required';
+                  }
+                  return null;
+                },
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'When is your birthday',
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'What time zone do you live in',
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'What Mental health problems do you suffer from'
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Update your Bio'
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 15),
+              child: RaisedButton(
+                onPressed: () {
+                  // Validate will return true if the form is valid, or false if
+                  // the form is invalid
+                  if (_formKey.currentState.validate()) {
+                    // Process data.
+                  }
+                },
+                child: Text('Submit'),
+              ),
+            ),
+          ],
         ),
-              TextFormField(
-          decoration: const InputDecoration(
-            hintText: 'Enter yourha',
-          ),
-          validator: (value) {
-            if (value.isEmpty) {
-              return 'Please enter sasome text';
-            }
-            return null;
-          },
-          
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 16.0),
-          child: RaisedButton(
-            onPressed: () {
-              // Validate will return true if the form is valid, or false if
-              // the form is invalid.
-              if (_formKey.currentState.validate()) {
-                // Process data.
-              }
-            },
-            child: Text('Submit'),
-          ),
-        ),
-      ],
-    ),
-  )
-  ),
-  );
+      )
+    );
   }
 }
