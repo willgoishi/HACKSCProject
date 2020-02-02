f043f8a513d8f7cba5dcb8f04e99a4ca891cf4ee
willgoishi <william3296@mail.fresnostate.edu>
2020-02-01 21:12:31 -0800

    updated submit functionality when creating a user entity - user is saved to firebase

--- hackscproject/lib/usercreationform.dart
+++ hackscproject/lib/usercreationform.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() => runApp(UserCreationForm());
+
+class UserModel
+{
+    final String firstName;
+    final String lastName;
+    final String mentalHealthIssue;
+    final String bioInformation; 
+    //final DateTime birthDate;
+
+    UserModel(this.firstName, this.lastName, this.mentalHealthIssue, this.bioInformation);
+    //, this.birthDate
+}
 
 /// This Widget is the main application widget.
 class UserCreationForm extends StatelessWidget {
@@ -17,13 +30,56 @@ class UserCreationForm extends StatelessWidget {
     );
   }
 }
+
 class MyStatefulWidget extends StatefulWidget {
   MyStatefulWidget({Key key}) : super(key: key);
   @override
   _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
 }
+
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   final _formKey = GlobalKey<FormState>();
+  final firebaseReference = Firestore.instance;
+
+  final myFirstNameController = TextEditingController();
+  final myLastNameController = TextEditingController();
+  final myMentalHealthController = TextEditingController();
+  final myBioInfoController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    myFirstNameController.dispose();
+    myLastNameController.dispose();
+    myMentalHealthController.dispose();
+    myBioInfoController.dispose();
+    super.dispose();
+  }
+
+  void submission()
+  {
+      var user = new UserModel(
+        myFirstNameController.text,
+        myLastNameController.text,
+        myMentalHealthController.text,
+        myBioInfoController.text
+      );
+
+      firebaseReference.collection("users").add(
+        {
+          "First Name" : user.firstName,
+          "Last Name" : user.lastName,
+          "Mental Health" : user.mentalHealthIssue,
+          "Bio Info" : user.bioInformation,
+        }
+      )
+      .catchError(() => {
+        //Do someting when error occurs
+      });
+
+      
+  }
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -35,6 +91,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
             Padding(
               padding : EdgeInsets.all(10),
               child: TextFormField(
+                controller: myFirstNameController,
                 decoration: const InputDecoration(
                   hintText: 'Enter name or Alias ',
                 ),
@@ -49,6 +106,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
             Padding(
               padding: EdgeInsets.all(10),
               child: TextFormField(
+                controller: myLastNameController,
                 decoration: const InputDecoration(
                   hintText: 'When is your birthday',
                 ),
@@ -77,6 +135,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
             Padding(
               padding: EdgeInsets.all(10),
               child: TextFormField(
+                controller: myMentalHealthController,
                 decoration: InputDecoration(
                   labelText: 'What Mental health problems do you suffer from'
                 ),
@@ -85,6 +144,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
             Padding(
               padding: EdgeInsets.all(10),
               child: TextFormField(
+                controller: myBioInfoController,
                 decoration: InputDecoration(
                   labelText: 'Update your Bio'
                 ),
@@ -97,7 +157,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
                   // Validate will return true if the form is valid, or false if
                   // the form is invalid
                   if (_formKey.currentState.validate()) {
-                    // Process data.
+                    submission();
                   }
                 },
                 child: Text('Submit'),
